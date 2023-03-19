3bcd8b5df43e3386d9672ab8316127c618092159
QuincyClain <pesotskiyvlad9@gmail.com>
2023-03-19 21:04:14 +0300

    started LR2

---
+++ LR2/2.sql
@@ -0,0 +1,29 @@
+CREATE SEQUENCE students_seq;
+CREATE OR REPLACE TRIGGER unique_student
+BEFORE INSERT
+ON STUDENTS
+FOR EACH ROW
+DECLARE
+counter number;
+BEGIN
+IF :new.id IS NOT NULL THEN
+select count(*) as c into counter
+
+from students
+where id = :new.id;
+IF counter > 0 THEN
+raise_application_error(-300, 'your student id is not unique.');
+END IF;
+ELSE
+counter := 1;
+while counter > 0
+LOOP
+SELECT students_seq.nextval
+INTO :new.id
+FROM dual;
+select count(*) as c into counter
+from students
+where id = :new.id;
+END LOOP;
+END IF;
+END;
